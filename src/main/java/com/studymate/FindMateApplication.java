9db1657ef56a0206494221f835338c1c88385e3e
countmonte7 <countmonte7@naver.com>
2020-08-25 20:42:12 +0900

    setup and signup html page

---
+++ src/main/java/com/studymate/FindMateApplication.java
@@ -0,0 +1,13 @@
+package com.studymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMateApplication.class, args);
+	}
+
+}

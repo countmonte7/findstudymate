53681afb3137ee540a0aeedc17b3793b92b94b82
countmonte7 <countmonte7@naver.com>
2020-09-23 13:13:46 +0900

    header, navigation repitition removed. HomeController added. html file to dynamic page. accessing to html through controller. update user info added.

--- src/main/java/com/studymate/web/UserController.java
+++ src/main/java/com/studymate/web/UserController.java
@@ -1,12 +1,12 @@
 package com.studymate.web;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,11 +21,16 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+
 	@GetMapping("/signUp")
 	public String signUp() {
 		return "/user/form";
 	}
 	
+	@GetMapping("/signIn")
+	public String signIn() {
+		return "/user/login";
+	}
 
 	@PostMapping("")
 	public String create(User user) {
@@ -37,5 +42,12 @@ public class UserController {
 	public String showList(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateform(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).orElse(null);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
 }
